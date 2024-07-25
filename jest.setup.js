af3e86fb7c1e031e068f0e139b0a62f0505ffb65
Klee Collage <klee.collage@gmail.com>
2024-07-25 13:31:57 -0500

    Testing setup
    Dependencies for testing with jest + vite added; plus testing axios in calendarApi

---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+require('dotenv').config({
+    path: '.env.test'
+});
+// Realizar el mock completo de las variables de entorno
+jest.mock('./src/helpers/getEnvVariables', () => ({
+    getEnvVariables: () => ({ ...process.env })
+}));
---
+++ tests/api/calendarApi.test.js
@@ -0,0 +1,17 @@
+import { expect } from "@jest/globals"
+import calendarApi from "../../src/api/calendarApi"
+
+describe('Pruebas en calendarApi', () => { 
+    // ************************************************************ //
+    test('debe tener la configuracion por defecto', () => {
+        expect(calendarApi.defaults.baseURL).toBe(process.env.VITE_API_URL);
+    });
+    // ************************************************************ //
+    test('debe tener el x-token en el header de todas las peticiones', async () => {
+        const token = 'ABC-123-XYZ'
+        localStorage.setItem('token', token);
+        const res = await calendarApi.get('/auth');
+        
+        expect(res.config.headers['x-token']).toBe(token);
+    });
+ })
